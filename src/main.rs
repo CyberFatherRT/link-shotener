a8e836756420d93d205b597a692bae30f69fe6c9
CyberFather <maks12ate@gmail.com>
2023-10-08 13:19:00 +0300

    start web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    http,
+    routing::{get, Router},
+};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let port = std::env::var("PORT")?;
+
+    let app = Router::new().route("/", get(index));
+
+    axum::Server::bind(&format!("0.0.0.0:{port}").parse()?)
+        .serve(app.into_make_service())
+        .await?;
+
+    Ok(())
+}
+
+async fn index() -> http::StatusCode {
+    http::StatusCode::IM_A_TEAPOT
 }
